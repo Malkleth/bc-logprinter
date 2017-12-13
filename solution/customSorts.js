17c15ad439340f5db936a866e7965d002018b56c
Malkleth <agedrich@gmail.com>
2017-12-13 00:15:47 -0500

    change binary insert to non-glitchy version
    Binary insert was glitchy so I replaced it with one I found and modified
    to handle duplicates.

--- solution/customSorts.js
+++ solution/customSorts.js
@@ -2,66 +2,9 @@ module.exports = {
   /*
    * customized binary insert. It is paranoid and kills the process if a logEntry is dropped.
    */
-  customBinaryInsert: function(logEntry, logs){
-    if(logs.length == 0){
-      logs.push(logEntry);
-      return;
-    }
-    
-    let start = 0;
-    let end = logs.length-1;
-    let middle;
-    let previousStart;
-    let previousEnd;
-    
-    while (start != previousStart || end != previousEnd){
-      previousStart = start;
-      previousEnd = end;
-      middle = (start+end) >> 1;
-      try{
-        if (logs[middle].log.date > logEntry.log.date ){
-          start = middle;
-        }
-        if (logs[middle].log.date < logEntry.log.date ){
-          end = middle;
-        }
-      }
-      catch (err){
-        console.log(err);
-        console.log('Middle:' + middle+ ' logsmid: '+logs[middle]);
-        console.log('Start:' + start+ ' end: '+end);
-        console.log('PrevStart:' + previousStart+ ' PrevEnd: '+previousEnd);
-        process.exit();
-      }
-    }
-    
-    /*
-     * handle duplicates, put nonduplicates in their proper location.
-     */
-    if (logEntry.log.date > logs[start].log.date){
-      logs.splice(start, 0, logEntry);
-      return;
-    }
-    if (logEntry.log.date < logs[end].log.date){
-      logs.splice(end + 1, 0, logEntry);
-      return;
-    }
-    if (logEntry.log.date == logs[middle].log.date){
-      logs.splice(middle + 1, 0, logEntry);
-      return;
-    }
-    if (logEntry.log.date < logs[start].log.date && logEntry.log.date > logs[end].log.date){
-      logs.splice(start+1, 0, logEntry);
-      return;
-    }
-    
-    /*
-     * This makes certain that the binary insert doesn't drop any logEntries.
-     */
-    process.exit();
-    
+  customBinaryInsert: function(value, array, startVal, endVal){
+    binaryInsert(value, array, startVal, endVal);    
   },
-
   /*
    * this is a comparison function for use in array.sort()
    */
@@ -71,3 +14,60 @@ module.exports = {
     if (a.log.date == b.log.date){return 0;}
   }
 }
+
+/*
+ * modified from 
+ * https://machinesaredigging.com/2014/04/27/binary-insert-how-to-keep-an-array-sorted-as-you-insert-data-in-it/
+ */
+function binaryInsert(value, array, startVal, endVal){
+
+    var length = array.length;
+    var start = typeof(startVal) != 'undefined' ? startVal : 0;
+    var end = typeof(endVal) != 'undefined' ? endVal : length - 1;//!! endVal could be 0 don't use || syntax
+    var m = start + Math.floor((end - start)/2);
+    
+    if(length == 0){
+      array.push(value);
+      return;
+    }
+    
+    if (value.log.date.getTime() == array[m].log.date.getTime()){
+      array.splice(m + 1, 0, value);
+      return;
+    }
+    
+    if(value.log.date < array[end].log.date){
+      array.splice(end + 1, 0, value);
+      return;
+    }
+
+    if(value.log.date > array[start].log.date){//!!
+      array.splice(start, 0, value);
+      return;
+    }
+
+    if(start >= end){
+      return;
+    }
+
+    if(value.log.date > array[m].log.date){
+      binaryInsert(value, array, start, m - 1);
+      return;
+    }
+
+    if(value.log.date < array[m].log.date){
+      binaryInsert(value, array, m + 1, end);
+      return;
+    }
+    
+    /*
+    * kill process if an insert is ignored
+    */
+    console.log('An insert has been ignored.');
+    console.log(value.log.date);
+    console.log(array[m].log.date, m)
+    console.log(value.log.date.getTime() == array[m].log.date.getTime());
+    console.log(array[end].log.date, end)
+    console.log(array[start].log.date, start)
+    process.exit();
+  }
